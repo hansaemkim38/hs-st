56d314c663a60c44d8b2f0e0aee582987f62e9da
kim-han-saem <khs_38@naver.com>
2025-05-04 12:03:10 +0900

    feat: next15 sec03 - page rounting

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,11 @@
-export default function Search() {
+export default async function Search({ searchParams }: { searchParams: Promise<{ q: string }> }) {
+	console.log('searchParams', searchParams);
+	console.log('searchParams', await searchParams);
+	const { q } = await searchParams;
 	return (
 		<div>
 			<h1>Search</h1>
-			<p>This is the search page.</p>
+			<p>search page : {q}</p>
 		</div>
 	);
 }
